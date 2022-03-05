0a36c1c05baa8190642f3509d71b82bc31507107
Santiago Padilla <santiagopadilla117@gmil.com>
2022-03-05 16:27:59 -0500

    config react tsx routerv6

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,59 @@
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, NavLink, Navigate } from "react-router-dom";
+
+import logo from "../logo.svg";
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="flex ">
+        <nav className="flex flex-col justify-start items-center bg-purple-900 h-screen">
+          <img className="h-32 mt-24" src={logo} alt="react logo" />
+
+          <ul className="flex flex-col gap-y-4 text-2xl">
+            <li>
+              <NavLink
+                to="/home"
+                className={({ isActive }) => (isActive ? "text-gray-400" : "")}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? "text-gray-400" : "")}
+              >
+                about
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/other"
+                className={({ isActive }) => (isActive ? "text-gray-400" : "")}
+              >
+                other
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route
+            path="other"
+            element={<h1 className="flex-1">Other element</h1>}
+          />
+          <Route
+            path="about"
+            element={<h1 className="flex-1">Abouet element</h1>}
+          />
+          <Route
+            path="home"
+            element={<h1 className="flex-1">Home element</h1>}
+          />
+          <Route path="/*" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
